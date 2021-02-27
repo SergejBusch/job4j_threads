cdfa6b0971d5f18021161a917f4b63b12df9d9a6
SergejBusch <herimvsem@gmail.com>
2021-02-27 20:58:01 +0100

    State of thread

--- src/main/java/ru/job4j/concurrent/ConcurrentOutput.java
+++ src/main/java/ru/job4j/concurrent/ConcurrentOutput.java
@@ -2,14 +2,22 @@ package ru.job4j.concurrent;
 
 public class ConcurrentOutput {
     public static void main(String[] args) {
-        Thread thread = new Thread(
+        Thread first = new Thread(
                 () -> System.out.println(Thread.currentThread().getName())
         );
-        thread.start();
+        first.start();
         Thread second = new Thread(
                 () -> System.out.println(Thread.currentThread().getName())
         );
         second.start();
-        System.out.println(Thread.currentThread().getName());
+        var terminated = Thread.State.TERMINATED;
+        while (first.getState() != terminated) {
+            System.out.println("i'm wait");
+        }
+        while (second.getState() != terminated) {
+            System.out.println("i'm wait too");
+        }
+        System.out.println("work completed");
+
     }
 }

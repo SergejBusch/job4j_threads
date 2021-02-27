e65f8acff5788747a6e75d6eea03a6b223d417fe
SergejBusch <herimvsem@gmail.com>
2021-02-27 11:33:45 +0100

    Thread start

---
+++ src/main/java/ru/job4j/concurrent/ConcurrentOutput.java
@@ -0,0 +1,15 @@
+package ru.job4j.concurrent;
+
+public class ConcurrentOutput {
+    public static void main(String[] args) {
+        Thread thread = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+        thread.start();
+        Thread second = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+        second.start();
+        System.out.println(Thread.currentThread().getName());
+    }
+}

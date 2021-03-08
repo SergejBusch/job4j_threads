85bf39281f98b08f01e777d62e21a17f39c8c56e
SergejBusch <herimvsem@gmail.com>
2021-03-08 02:39:23 +0100

    UserStorage, remove AtomicLong time

--- src/main/java/ru/job4j/concurrent/threads/WgetDownload.java
+++ src/main/java/ru/job4j/concurrent/threads/WgetDownload.java
@@ -4,12 +4,10 @@ import java.io.BufferedInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
-import java.util.concurrent.atomic.AtomicLong;
 
 public class WgetDownload implements Runnable {
     private final String url;
     private final int speed;
-    private final AtomicLong atomicLong = new AtomicLong();
 
     public WgetDownload(String url, int speed) {
         this.url = url;
@@ -22,10 +20,11 @@ public class WgetDownload implements Runnable {
              FileOutputStream fileOutputStream = new FileOutputStream("pom_tmp.xml")) {
             int bufferSize = 1024;
             byte[] dataBuffer = new byte[bufferSize];
-            int bytesRead;
-            while ((bytesRead = read(dataBuffer, in)) != -1) {
-                fileOutputStream.write(dataBuffer, 0, bytesRead);
-                long sleepTime = getSleepTime(bufferSize);
+            long[] results;
+            while ((results = (read(dataBuffer, in)))[0] != -1) {
+                fileOutputStream.write(dataBuffer, 0, (int) results[0]);
+                long time = calculateTime(results[1], results[2]);
+                long sleepTime = getSleepTime(bufferSize, time);
                 if (sleepTime > 0) {
                     Thread.sleep(sleepTime);
                 }
@@ -38,21 +37,19 @@ public class WgetDownload implements Runnable {
         }
     }
 
-    private int read(byte[] dataBuffer, BufferedInputStream in) throws IOException {
+    private long[] read(byte[] dataBuffer, BufferedInputStream in) throws IOException {
         int bytesRead;
         long begin = System.currentTimeMillis();
         bytesRead = in.read(dataBuffer, 0, 1024);
         long end = System.currentTimeMillis();
-        calculateTime(begin, end);
-        return bytesRead;
+        return new long[] {bytesRead, begin, end};
     }
 
-    private void calculateTime(long begin, long end) {
-        atomicLong.set(end - begin);
+    private long calculateTime(long begin, long end) {
+        return end - begin;
     }
 
-    private long getSleepTime(int numberOfBytes) {
-        long time = atomicLong.get();
+    private long getSleepTime(int numberOfBytes, long time) {
         if (numberOfBytes / time > speed) {
            return time - (speed / numberOfBytes);
         }
